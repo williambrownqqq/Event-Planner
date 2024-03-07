bc2a1dcc24f39b27ebd9dec6de21fc50354749bc
lesha <iamalexzanchenko@gmail.com>
2024-03-07 15:42:38 +0200

    solve the problem

--- authentication/src/main/java/com/zanchenko/alex/diploma/dto/payload/response/JwtResponse.java
+++ authentication/src/main/java/com/zanchenko/alex/diploma/dto/payload/response/JwtResponse.java
@@ -1,25 +1,19 @@
 package com.zanchenko.alex.diploma.dto.payload.response;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
-@Getter
-@Setter
-//@NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
+//@Getter // make a problem for login
+//@Setter
 public class JwtResponse {
-    String token;
-    String type ="Bearer";
-    Long id;
-    String username;
-    String email;
-    List<String> roles;
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
 
     public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
         this.token = accessToken;
@@ -27,5 +21,49 @@ public class JwtResponse {
         this.username = username;
         this.email = email;
         this.roles = roles;
+    }
+
+    public String getAccessToken() {
+        return token;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getTokenType() {
+        return type;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.type = tokenType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
     }
 }
